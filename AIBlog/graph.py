0fb02062676e8e740f56a4641d7d4b7328215c79
Abozar <abozar.alizadeh@gmail.com>
2025-11-28 15:04:50 +0100

    Update tools in get_react_agent and add Cython dependency
    Replaces the web search tools list in get_react_agent with a dictionary-based tool and updates requirements.txt to include Cython. Also ensures a newline at the end of requirements.txt.

--- AIBlog/graph.py
+++ AIBlog/graph.py
@@ -25,7 +25,8 @@ if "AZURE_OPENAI_ENDPOINT" not in os.environ:
 async def get_react_agent():
     savetitletool = set_title
     imagetool = get_image_by_text
-    tools = [tavilysearchinternettool, ddgsearchinternettool, imagetool, savetitletool]
+    #tools = [tavilysearchinternettool, ddgsearchinternettool, imagetool, savetitletool]
+    tools = [{ type: "web_search" }, imagetool, savetitletool]
     tools += await get_browsewebtools()
 
     max_input_tokens = int(os.environ.get("AZURE_OPENAI_MAX_INPUT_TOKENS", "270000"))
